d6927e5db6393c8dcaff2f11f2098f3e7d078c86
Xe-Persistent <474663082@qq.com>
2026-01-12 21:05:25 +0800

    :fire: Remove duplicate AkagiApp import from core init

--- akagi_ng/core/__init__.py
+++ akagi_ng/core/__init__.py
@@ -1,11 +1,9 @@
 from akagi_ng.core import context, paths
-from akagi_ng.core.app import AkagiApp
 from akagi_ng.core.event_handler import NotificationHandler
 from akagi_ng.core.logging import configure_logging, logger
 from akagi_ng.core.notification_codes import NotificationCode
 
 __all__ = [
-    "AkagiApp",
     "NotificationHandler",
     "NotificationCode",
     "configure_logging",
